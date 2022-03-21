9cd40293f381781c38555ad364a372084b3c42f4
PouletRaPHOO <raphael.herzlich@icloud.com>
2022-03-21 17:04:03 +0100

    Base

---
+++ main.py
@@ -0,0 +1,12 @@
+class Pos :
+    def __init__(self,x,y) :
+        self.x = x
+        self.y = y
+
+def C
+
+
+class Grille :
+    def __init__(self,click:Pos,width:int,height:int,b_num:int) :
+        self.grid = [[Case()for k ]for i in range]
+        
